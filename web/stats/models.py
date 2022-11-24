ba5a1a79e1e1f8fdf52514a23222df94c4aee945
Przemyslaw Bialon <pbialon@sumologic.com>
2022-11-24 23:29:39 +0100

    prepared essential models

--- web/stats/models.py
+++ web/stats/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.date.strftime("%Y-%m-%d %H:%M")
+
+
+class Stats(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    goals = models.IntegerField()
+    assists = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.game}: {self.player} -> ({self.goals}, {self.assists})"
